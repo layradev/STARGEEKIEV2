1557662ebc864102aa447dcd20fdf80f35fae595
layra caroline <layra.ferreira@portalsesisp.org.br>
2023-05-30 09:11:07 -0300

    logica 30/05/2023

--- login.js
+++ login.js
@@ -5,11 +5,20 @@ const formulario = document.getElementById("formulario");
 
 formulario.onsubmit = (evento) => {
     let dados = JSON.parse(localStorage.getItem("dados"));
-
+    let logado;
     dados.forEach(elemento => {
         if (elemento.email === email.value && elemento.senha === senha.value){
+            let dados = JSON.parse(sessionStorage.getItem("logado")) || [];
+            dados.push(
+                {
+                    email : email.value,
+                }
+            )
+            sessionStorage.setItem("logado", JSON.stringify(dados));
             evento.preventDefault();
             mensagem.innerHTML = "logado";
+            logado = true;
+            window.location.assign("catalogo.html");
             return true;
     
         } else {
